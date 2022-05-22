b621ca59fda4f1bf97e34391a0962f98bf5bcfa4
bayuik <bayuindrakusuma05@gmail.com>
2022-05-22 22:52:37 +0700

    refactor cta

--- src/components/Cta/index.js
+++ src/components/Cta/index.js
@@ -3,10 +3,12 @@ import { Button, Card, Container } from "react-bootstrap";
 const Cta = () => {
   return (
     <Container>
-      <Card className="text-center">
+      <Card className="text-center bgBlue text-white p-5">
         <Card.Body>
-          <Card.Title>Sewa Mobil di (Lokasimu) Sekarang</Card.Title>
-          <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card.Text>
+          <h2 className="fw-bold">Sewa Mobil di (Lokasimu) Sekarang</h2>
+          <Card.Body className="mb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br /> tempor incididunt ut labore et dolore magna aliqua.
+          </Card.Body>
           <Button variant="success">Mulai Sewa Mobil</Button>
         </Card.Body>
       </Card>
