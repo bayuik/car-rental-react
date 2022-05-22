2bf92c45bceb0830d5ae975e5c94b146f3ffca1d
bayuik <bayuindrakusuma05@gmail.com>
2022-05-23 00:11:46 +0700

    reafactor hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,23 +1,17 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 const Hero = () => {
   return (
-    <>
-      <div className="bgGreen">
-        <Container>
-          <Row>
-            <Col>
-              <h2>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h2>
-              <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
-              <Button variant="success">Mulai Sewa Mobil</Button>
-            </Col>
-            <Col>
-              <img src="/images/img_car.png" alt="Car Logo" />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
+    <Row className="bgGreen mb-section">
+      <Col className="my-auto text-hero">
+        <h2 className="fw-bold mb-4">Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h2>
+        <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+        <Button variant="success">Mulai Sewa Mobil</Button>
+      </Col>
+      <Col>
+        <img src="/images/img_car.png" alt="Car Logo" />
+      </Col>
+    </Row>
   );
 };
 
